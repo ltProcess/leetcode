aed51188081bdf4982f0dba96a4d9942419e43a8
ltProcess <yunbei.lt@alibaba-inc.com>
2020-07-01 19:50:12 +0800

    feat: 二叉树dfs最大深度

---
+++ BinaryTree/104-maxDepth.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+// 广度优先搜索
+const maxDepth = function(root) {
+  if (root === null) return 0;
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
