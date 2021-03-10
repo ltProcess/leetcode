c5b6352322fafe989178e4c19609c6699baa2f16
ltProcess <lt41156004@163.com>
2021-03-10 15:16:48 +0800

    feat: removeDuplicates递归

---
+++ 1047.删除字符串中的所有相邻重复项.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=1047 lang=javascript
+ *
+ * [1047] 删除字符串中的所有相邻重复项
+ */
+
+// @lc code=start
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var removeDuplicates = function(S) {
+  const strList = S.split('');
+  const len = strList.length;
+  let flag = false;
+  const processList = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === arr[i + 1]) {
+        flag = true;
+        arr.splice(i, 2);
+        break;
+      }
+      flag = false;
+    }
+  }
+  processList(strList);
+  while (flag && strList.length > 1) {
+    processList(strList);
+  }
+  return strList.join('');
+};
+// @lc code=end
+
+// const ss = removeDuplicates('aaaaaaaa');
+// console.log(ss);
