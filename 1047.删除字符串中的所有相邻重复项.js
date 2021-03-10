308453bb15d82e3e37a99e378d70da4a747a2544
ltProcess <lt41156004@163.com>
2021-03-10 17:01:10 +0800

    feat: stack removeDuplicates

--- 1047.删除字符串中的所有相邻重复项.js
+++ 1047.删除字符串中的所有相邻重复项.js
@@ -10,24 +10,38 @@
  * @return {string}
  */
 var removeDuplicates = function(S) {
-  const strList = S.split('');
-  const len = strList.length;
-  let flag = false;
-  const processList = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === arr[i + 1]) {
-        flag = true;
-        arr.splice(i, 2);
-        break;
-      }
-      flag = false;
+  // const strList = S.split('');
+  // const len = strList.length;
+  // let flag = false;
+  // const processList = (arr) => {
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i] === arr[i + 1]) {
+  //       flag = true;
+  //       arr.splice(i, 2);
+  //       break;
+  //     }
+  //     flag = false;
+  //   }
+  // }
+  // processList(strList);
+  // while (flag && strList.length > 1) {
+  //   processList(strList);
+  // }
+  // return strList.join('');
+
+  const stack = [];
+  for (let i = 0; i < S.length; i++) {
+    if (i === 0) {
+      stack.push(S[0]);
+      continue;
+    }
+    if (stack[stack.length - 1] === S[i]) {
+      stack.pop();
+    } else {
+      stack.push(S[i]);
     }
   }
-  processList(strList);
-  while (flag && strList.length > 1) {
-    processList(strList);
-  }
-  return strList.join('');
+  return stack.join('');
 };
 // @lc code=end
 
