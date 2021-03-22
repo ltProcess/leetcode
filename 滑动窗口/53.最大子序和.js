65db10f20dd81973eb584f5632e64fafae102d84
ltProcess <lt41156004@163.com>
2021-03-22 13:31:17 +0800

    feat: 最大子序和

--- 滑动窗口/53.最大子序和.js
+++ 滑动窗口/53.最大子序和.js
@@ -10,7 +10,22 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-
+  const len = nums.length;
+  if (len === 1) return nums[0];
+  let temp = null;
+  let i = 0;
+  let result = null;
+  while (i < len) {
+    if (i === 0) result = nums[0];
+    temp += nums[i];
+    if (temp < nums[i]) {
+      temp = nums[i];
+    }
+    result = Math.max(temp, result);
+    i++;
+  }
+  return result;
 };
+// console.log(maxSubArray([5,4,-1,7,8]))
 // @lc code=end
 
