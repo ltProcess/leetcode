8132d67d57aa72a162e6cc6a0b33d9a9fe3d3b3c
ltProcess <lt41156004@163.com>
2021-04-10 18:06:56 +0800

    fix: lint

--- 前缀和/1109.航班预订统计.js
+++ 前缀和/1109.航班预订统计.js
@@ -13,20 +13,22 @@
 var corpFlightBookings = function(bookings, n) {
   let addMap = new Map();
   let minusMap = new Map();
-  for(let i=0;i<bookings.length;i++){
-      let [start,end,tickets]=bookings[i];
-      addMap.set(start,(addMap.get(start)||0)+tickets);
-      minusMap.set(end,(minusMap.get(end)||0)+tickets);
+  for(let i = 0;i < bookings.length; i++){
+    let [start, end, tickets] = bookings[i];
+    addMap.set(start,(addMap.get(start)||0)+tickets);
+    minusMap.set(end,(minusMap.get(end)||0)+tickets);
   }
-  let sum=0;
-  let ans=new Array(n);
-  for(let i=0;i<n;i++){
-      sum+=addMap.get(i+1)||0;
-      ans[i]=sum;
-      sum-=minusMap.get(i+1)||0;
+  // console.log(addMap, minusMap);
+  let sum = 0;
+  let ans= new Array(n);
+  for(let i = 0;i < n; i++){
+    sum += addMap.get(i+1)||0;
+    ans[i] = sum;
+    sum -= minusMap.get(i+1)||0;
   }
   return ans;
 };
-
+// const a = corpFlightBookings([[1,2,10],[2,3,20],[2,5,25]], 5);
+// console.log(a);
 // @lc code=end
 
