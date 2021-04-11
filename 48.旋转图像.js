7c3b3646a46b4f5a0acf312b43c33aa2d35f5653
ltProcess <lt41156004@163.com>
2021-04-11 11:46:39 +0800

    feat: 操作数组

--- 48.旋转图像.js
+++ 48.旋转图像.js
@@ -9,8 +9,20 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var rotate = function(matrix) {
-
+ var rotate = function(matrix) {
+  const len = matrix.length;
+  if (len=== 1) return matrix;
+  const result = new Array(len);
+  for (let k = 0; k < len; k++) {
+    result[k] = [];
+  }
+  for (let i = 0; i < len; i++) {
+    const current = matrix[i];
+    for (let j = 0; j < current.length; j++) {
+      result[j].unshift(current[j]);
+    }
+  }
+  return result;
 };
 // @lc code=end
 
