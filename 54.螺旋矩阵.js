1e0a2e766787f61e1270275e144484c8f772cafa
ltProcess <lt41156004@163.com>
2021-04-12 10:50:31 +0800

    feat: 递归螺旋矩阵

--- 54.螺旋矩阵.js
+++ 54.螺旋矩阵.js
@@ -10,7 +10,35 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-  const len = matrix.length;
+  const traverse = (arr, result) => {
+    const n = arr.length;
+
+    for (let i = 0; i < n; i++) {
+      if (i === 0) {
+        result = result.concat(arr[i]);
+      } else if (i === n - 1) {
+        result = result.concat(arr[i].reverse());
+      } else {
+        if (arr[i].length) {
+          result.push(arr[i].pop());
+        }
+      }
+    }
+    arr.pop();
+    arr.shift();
+    for (let j = n - 3; j >= 0; j--) {
+      if (arr[j].length) {
+        result.push(arr[j].shift());
+      }
+    }
+    if (arr.length) {
+      return traverse(arr, result);
+    } else {
+      return result;
+    }
+  }
+  
+  return traverse(matrix, []);
 };
 // @lc code=end
 
