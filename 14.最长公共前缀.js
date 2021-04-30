b789a5b254ed0143b81099b0e6e0ac0818a52f4e
ltProcess <lt41156004@163.com>
2021-04-30 14:33:51 +0800

    feat: long string

---
+++ 14.最长公共前缀.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=javascript
+ *
+ * [14] 最长公共前缀
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  let res = '';
+  if (!strs) return '';
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== strs[0][i]) return res;
+    }
+    res += strs[0][i];
+  }
+  return res;
+};
+// @lc code=end
+
