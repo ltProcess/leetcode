2674df3d5ae5ee8b82a2c7a0c6f7f7122ad44305
ltProcess <lt41156004@163.com>
2022-09-04 18:09:16 +0800

    feat: 25

---
+++ 链表/25.k-个一组翻转链表.js
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=javascript
+ *
+ * [25] K 个一组翻转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+
+// 时间复杂度O(n)
+// 空间复杂度O(1)
+const myReverse = (head, tail) => {
+  let prev = tail.next;
+  let p = head;
+  while (prev !== tail) {
+    const next = p.next;
+    p.next = prev;
+    prev = p;
+    p = next;
+  }
+  return [tail, head];
+};
+var reverseKGroup = function (head, k) {
+  const hair = new ListNode(0);
+  hair.next = head;
+  let pre = hair;
+
+  while (head) {
+    let tail = pre;
+    // 查看剩余部分长度是否大于等于 k
+    for (let i = 0; i < k; i++) {
+      tail = tail.next;
+      if (!tail) return hair.next;
+    }
+    const nex = tail.next;
+    [head, tail] = myReverse(head, tail);
+    // 把子链表重新接回原链表
+    pre.next = head;
+    tail.next = nex;
+    pre = tail;
+    head = tail.next;
+  }
+  return hair.next;
+};
+// @lc code=end
