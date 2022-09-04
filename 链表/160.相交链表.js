7dd1b9cd89c19c59af286a8210816bc7f896b53a
ltProcess <lt41156004@163.com>
2022-09-04 16:39:15 +0800

    feat: 160

---
+++ 链表/160.相交链表.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=javascript
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  if (headA === null || headB === null) return null;
+  let pA = headA,
+    pB = headB;
+  while (pA !== pB) {
+    pA = pA === null ? headB : pA.next;
+    pB = pB === null ? headA : pB.next;
+  }
+  return pB;
+};
+// @lc code=end
