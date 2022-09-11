fb909584e9e83246e91b0a16d54497b9c90249d1
ltProcess <lt41156004@163.com>
2022-09-11 17:40:23 +0800

    fix: LTS注释

--- dp/300.最长递增子序列.js
+++ dp/300.最长递增子序列.js
@@ -14,6 +14,10 @@
 // 输入：nums = [10,9,2,5,3,7,101,18]
 // 输出：4
 // 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+
+// 思路：dp[i]表示选择nums[i]，并且以nums[i]结尾的最长上升子序列的长度。两层循环，i：1～nums.length
+// j：0～i，如果nums[i] > nums[j]，则构成一个上升对，dp[i]就从dp[i], dp[j]+1两个种选择较大者，最后返回dp数组总的最大数
+// 复杂度分析：时间复杂度O(n^2)，n是nums的长度，外层需要循环n次，dp[i]需要从dp[0~i-1],所以复杂度是O(n^2)。空间复杂度是O(n)，即dp数组的空间
 const lengthOfLIS = function (nums) {
   const len = nums.length;
   if (len === 0) return 0;
