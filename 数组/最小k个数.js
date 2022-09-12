abda9bfd2a644157b79601b62e2b5da3fa9b2d06
ltProcess <lt41156004@163.com>
2022-09-12 17:45:19 +0800

    feat: 快排2

--- 数组/最小k个数.js
+++ 数组/最小k个数.js
@@ -40,3 +40,22 @@ const smallestK = function (arr, k) {
   quicksort(0, arr.length - 1);
   return arr.slice(0, k);
 };
+
+var quickSort2 = function (arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  var pivotIndex = Math.floor(arr.length / 2);
+  var pivot = arr.splice(pivotIndex, 1)[0];
+  var left = [];
+  var right = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat([pivot], quickSort(right));
+};
